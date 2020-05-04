1571722efe233f6b5131e46be4fbfa9a3a764ada
David <temiloluwaw@gmail.com>
2020-05-04 18:58:42 +0100

    implemented the user domain

---
+++ src/main/java/com/qa/domain/User.java
@@ -0,0 +1,56 @@
+package com.qa.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long user_id;
+    private String username;
+    private String password;
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return getUser_id().equals(user.getUser_id()) &&
+                getUsername().equals(user.getUsername()) &&
+                getPassword().equals(user.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUser_id(), getUsername(), getPassword());
+    }
+}
