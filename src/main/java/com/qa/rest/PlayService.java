15fccb30a6142f8c6c88df3186aa4e065f0018f5
David <temiloluwaw@gmail.com>
2020-05-06 12:12:45 +0100

    plays services updated and complete for the CRUD functionalities

--- src/main/java/com/qa/rest/PlayService.java
+++ src/main/java/com/qa/rest/PlayService.java
@@ -1,9 +1,15 @@
 package com.qa.rest;
 
+import com.qa.domain.Plays;
+import com.qa.dto.PlayDTO;
+import com.qa.exception.PlayNotFoundException;
 import com.qa.repo.PlaysRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PlayService {
@@ -16,6 +22,38 @@ public class PlayService {
     public PlayService(PlaysRepository repo, ModelMapper mapper) {
         this.repo = repo;
         this.mapper = mapper;
+    }
+
+    public PlayDTO mapToDTO(Plays plays){
+        return this.mapper.map(plays, PlayDTO.class);
+    }
+
+    public List<PlayDTO> readPlays(){
+        return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+    }
+
+    public PlayDTO createPlays(Plays plays){
+        Plays tempPlay = this.repo.save(plays);
+        return this.mapToDTO(tempPlay);
+    }
+
+    public PlayDTO findPlayById(Long id){
+        return this.mapToDTO(this.repo.findById(id).orElseThrow(PlayNotFoundException::new));
+    }
+
+    public PlayDTO updatePlay(Long id, Plays plays){
+        Plays update = this.repo.findById(id).orElseThrow(PlayNotFoundException::new);
+        update.setDescription(plays.getDescription());
+        Plays tempPlay = this.repo.save(plays);
+        return this.mapToDTO(tempPlay);
+    }
+
+    public boolean deletePlay(Long id){
+        if(!this.repo.existsById(id)){
+            throw new PlayNotFoundException();
+        }
+        this.repo.deleteById(id);
+        return this.repo.existsById(id);
     }
 
 }
