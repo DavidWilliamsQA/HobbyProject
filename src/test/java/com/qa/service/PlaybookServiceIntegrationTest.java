03462dce66e7eb2c139fb2ebf5251eb43b095313
David <temiloluwaw@gmail.com>
2020-05-09 19:10:50 +0100

    added the add play to playbook test for the playbook service integration test

--- src/test/java/com/qa/service/PlaybookServiceIntegrationTest.java
+++ src/test/java/com/qa/service/PlaybookServiceIntegrationTest.java
@@ -2,6 +2,7 @@ package com.qa.service;
 
 
 import com.qa.domain.Playbook;
+import com.qa.domain.Plays;
 import com.qa.dto.PlaybookDTO;
 import com.qa.repo.PlaybookRepository;
 import com.qa.repo.PlaysRepository;
@@ -32,11 +33,16 @@ public class PlaybookServiceIntegrationTest {
     private PlaybookRepository repository;
 
     @Autowired
+    private PlaysRepository playsRepository;
+
+    @Autowired
     private ModelMapper mapper;
 
     private Playbook updatePlaybook;
     private Playbook testPlaybook;
     private Playbook testPlaybookWithId;
+    private Plays testPlay;
+    private Long id = 1L;
 
     private PlaybookDTO mapToDTO(Playbook playbook){
         return this.mapper.map(playbook, PlaybookDTO.class);
@@ -46,8 +52,11 @@ public class PlaybookServiceIntegrationTest {
     public void setUp(){
         this.testPlaybook = new Playbook("test");
         this.updatePlaybook = new Playbook("update");
+        this.testPlay = new Plays("test play");
+        testPlay.setId(id);
         this.repository.deleteAll();
         this.testPlaybookWithId = this.repository.save(this.testPlaybook);
+        this.testPlay = this.playsRepository.save(this.testPlay);
     }
 
     @Test
@@ -76,5 +85,9 @@ public class PlaybookServiceIntegrationTest {
     @Test
     public void updatePlaybookTest(){
         assertNotEquals(this.service.updatePlaybook(testPlaybookWithId.getId(), updatePlaybook), this.mapToDTO(this.testPlaybookWithId));
+    }
+    @Test
+    public void addPlayToPlaybookTest(){
+        assertNotEquals(this.service.addPlays(testPlaybookWithId.getId(), testPlay.getId()), this.mapToDTO(this.testPlaybookWithId));
     }
 }
