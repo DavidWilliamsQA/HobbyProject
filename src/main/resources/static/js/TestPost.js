128c000214da6a9ba9a0b2fa968e2e996d50e980
David <temiloluwaw@gmail.com>
2020-05-14 17:16:57 +0100

    added the drop down option for the offence page

--- src/main/resources/static/js/TestPost.js
+++ src/main/resources/static/js/TestPost.js
@@ -1,25 +1,41 @@
-const REQ = new XMLHttpRequest();
+// const REQ = new XMLHttpRequest();
 
-function getTodos() {
-
-    REQ.onload = () => {
-        if (REQ.status === 200) {
-            //console.log(REQ);
-            console.log(REQ.response);
-            console.log(REQ.response.title);
-           // document.querySelector("#resp").outerHTML = REQ.response[0].description;
-           // document.createElement("option")
-        } else {
-            console.log(`Handle Error!`);
+const data = axios.get("http://localhost:8181/getAllPlaybooks").then(
+    res => {
+        console.log(data);
+        debugger;
+        const select = this.document.getElementById("playbooks");
+        for (let opt of res.data) {
+            const option = document.createElement("option");
+            option.value = opt.id;
+            option.textContent = opt.name;
+            select.appendChild(option);
         }
-    };
+    }
+    );
 
-    REQ.open("GET", "http://localhost:8181/getAllPlays");
-    REQ.setRequestHeader("Content-Type", "Application/json");
-    REQ.setRequestHeader("Access-Control-Allow-Origin", "*");
-    REQ.responseType = "json";
-    REQ.send();
-}
+// function getTodos() {
+//
+//     REQ.onload = () => {
+//         let data = `[{"id": "${document.getElementById("Playbook1").value}", "plays" }]`;
+//         let obj2 = JSON.parse(data);
+//         if (REQ.status === 200) {
+//             //console.log(REQ);
+//             console.log(REQ.response);
+//             console.log(REQ.response.title);
+//            // document.querySelector("#resp").outerHTML = REQ.response[0].description;
+//            // document.createElement("option")
+//         } else {
+//             console.log(`Handle Error!`);
+//         }
+//     };
+//
+//     REQ.open("PUT", "http://localhost:8181/addPlaysToPlaybook/");
+//     REQ.setRequestHeader("Content-Type", "Application/json");
+//     REQ.setRequestHeader("Access-Control-Allow-Origin", "*");
+//     REQ.responseType = "json";
+//     REQ.send();
+// }
 
 let butt1 = document.querySelector("#butt1");
 butt1.addEventListener("click", getTodos);
