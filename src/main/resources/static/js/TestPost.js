0fa2e883d2a5bfc3a4a479b2287e7d0ffe2630fc
David <temiloluwaw@gmail.com>
2020-05-14 18:22:27 +0100

    added an application properties file into the tests so that it uses the h2 and not mysql

--- src/main/resources/static/js/TestPost.js
+++ src/main/resources/static/js/TestPost.js
@@ -47,28 +47,6 @@ axios.get("http://localhost:8181/getAllPlaybooks").then(
     }
 );
 
-// function getTodos() {
-//
-//     REQ.onload = () => {
-//         let data = `[{"id": "${document.getElementById("Playbook1").value}", "plays" }]`;
-//         let obj2 = JSON.parse(data);
-//         if (REQ.status === 200) {
-//             //console.log(REQ);
-//             console.log(REQ.response);
-//             console.log(REQ.response.title);
-//            // document.querySelector("#resp").outerHTML = REQ.response[0].description;
-//            // document.createElement("option")
-//         } else {
-//             console.log(`Handle Error!`);
-//         }
-//     };
-//
-//     REQ.open("PUT", "http://localhost:8181/addPlaysToPlaybook/");
-//     REQ.setRequestHeader("Content-Type", "Application/json");
-//     REQ.setRequestHeader("Access-Control-Allow-Origin", "*");
-//     REQ.responseType = "json";
-//     REQ.send();
-// }
 
 let butt1 = document.querySelector("#butt1");
 butt1.addEventListener("click", getTodos);
