c12333798cd2d7c021f210c248dcb62d368ca94f
David <temiloluwaw@gmail.com>
2020-05-15 23:34:14 +0100

    added the functionality that allows you to see the list of playbooks available

--- src/main/resources/static/js/offence.js
+++ src/main/resources/static/js/offence.js
@@ -10,40 +10,6 @@ axios.get("http://localhost:8181/getAllPlaybooks").then(
     }
     );
 
-axios.get("http://localhost:8181/getAllPlaybooks").then(
-    res => {
-        const select = this.document.getElementById("playbooks2");
-        for (let opt of res.data) {
-            const option = document.createElement("option");
-            option.value = opt.id;
-            option.textContent = opt.name;
-            select.appendChild(option);
-        }
-    }
-);
-
-axios.get("http://localhost:8181/getAllPlaybooks").then(
-    res => {
-        const select = this.document.getElementById("playbooks3");
-        for (let opt of res.data) {
-            const option = document.createElement("option");
-            option.value = opt.id;
-            option.textContent = opt.name;
-            select.appendChild(option);
-        }
-    }
-);
-axios.get("http://localhost:8181/getAllPlaybooks").then(
-    res => {
-        const select = this.document.getElementById("playbooks4");
-        for (let opt of res.data) {
-            const option = document.createElement("option");
-            option.value = opt.id;
-            option.textContent = opt.name;
-            select.appendChild(option);
-        }
-    }
-);
 
 let butt1 = document.querySelector("#butt1");
 butt1.addEventListener("click", getTodos);
