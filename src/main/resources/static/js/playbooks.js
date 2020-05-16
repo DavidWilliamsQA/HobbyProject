316f8d085d19e3a07f591e499e2834b8c120e26c
David <temiloluwaw@gmail.com>
2020-05-16 13:30:05 +0100

    made a few changes to fix the issue of the drop downs to show the available playbooks on all the correct places

--- src/main/resources/static/js/playbooks.js
+++ src/main/resources/static/js/playbooks.js
@@ -5,48 +5,15 @@ axios.get("http://localhost:8181/getAllPlaybooks").then(
             const list = document.createElement("li");
             const aTag = document.createElement("a");
             aTag.textContent = opt.name;
-            // aTag.href = "#";
-            list.className = "nav-item";
+            aTag.href = "#";
+            // list.className = "nav-item";
             list.appendChild(aTag);
             select.appendChild(list)
         }
     }
     );
 
-axios.get("http://localhost:8181/getAllPlaybooks").then(
-    res => {
-        const select = this.document.getElementById("playbooks2");
-        for (let opt of res.data) {
-            const option = document.createElement("option");
-            option.value = opt.id;
-            option.textContent = opt.name;
-            select.appendChild(option);
-        }
-    }
-);
 
-axios.get("http://localhost:8181/getAllPlaybooks").then(
-    res => {
-        const select = this.document.getElementById("playbooks3");
-        for (let opt of res.data) {
-            const option = document.createElement("option");
-            option.value = opt.id;
-            option.textContent = opt.name;
-            select.appendChild(option);
-        }
-    }
-);
-axios.get("http://localhost:8181/getAllPlaybooks").then(
-    res => {
-        const select = this.document.getElementById("playbooks4");
-        for (let opt of res.data) {
-            const option = document.createElement("option");
-            option.value = opt.id;
-            option.textContent = opt.name;
-            select.appendChild(option);
-        }
-    }
-);
 
 let butt1 = document.querySelector("#butt1");
 butt1.addEventListener("click", getTodos);
